464bce642332bfe7d02c3fb36c48fb3c967e99b6
amandabox <amanda.box@mykenzie.snhu.edu>
2023-03-11 13:36:29 -0500

    adds controller, request, and response

---
+++ Application/src/main/java/com/kenzie/appserver/controller/EventController.java
@@ -0,0 +1,85 @@
+package com.kenzie.appserver.controller;
+
+import com.kenzie.appserver.controller.model.CreateEventRequest;
+import com.kenzie.appserver.controller.model.EventResponse;
+import com.kenzie.appserver.repositories.model.EventRecord;
+import com.kenzie.appserver.service.EventService;
+
+import com.kenzie.appserver.service.model.Event;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.UUID.randomUUID;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    private EventService eventService;
+
+    EventController(EventService eventservice){
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EventResponse> addNewEvent(@RequestBody CreateEventRequest createEvent) {
+
+        if (createEvent.getEventId() == null || createEvent.getEventId().length() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Event ID");
+        }
+        EventRecord newEvent = new EventRecord();
+        newEvent.setEventId(randomUUID().toString());
+        newEvent.setCustomerName(createEvent.getCustomerName().get());
+        newEvent.setDate(createEvent.getDate().get());
+        newEvent.setCustomerEmail(createEvent.getCustomerEmail().get());
+        newEvent.setStatus(createEvent.getStatus().get());
+
+        this.eventService.addNewEvent(newEvent);
+
+        EventResponse response = this.convertToResponse(newEvent);
+
+        return ResponseEntity.created(URI.create("/events/" + response.getEventId())).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponse>> getAllEvents() {
+        List<EventResponse> events = eventService.findAllEvents();
+        if (events == null || events.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponse> searchEventById(@PathVariable("id") String eventId) {
+        EventResponse response = eventService.getEvent(eventId);
+        if (response == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteEventById(@PathVariable("id") String eventId) {
+        eventService.deleteEvent(eventId);
+        return ResponseEntity.ok().build();
+    }
+
+    private EventResponse convertToResponse(EventRecord event){
+        EventResponse response = new EventResponse();
+        response.setEventId(event.getEventId());
+        response.setDate(event.getDate());
+        response.setStatus(event.getStatus());
+        response.setCustomerName(event.getCustomerName());
+        response.setCustomerEmail(event.getCustomerEmail());
+        return response;
+    }
+
+}
---
+++ Application/src/main/java/com/kenzie/appserver/controller/model/EventResponse.java
@@ -0,0 +1,62 @@
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EventResponse {
+    @JsonProperty("eventId")
+    private String eventId;
+
+    @JsonProperty("date")
+    private String date;
+
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("customerName")
+    private String customerName;
+
+    @JsonProperty("customerEmail")
+    private String customerEmail;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
