a0d7c1ec2621ab56be9fc3012d25d8542dbff7c7
jarrettmueller <jarrett.mueller@mykenzie.snhu.edu>
2023-03-16 09:55:10 -0400

    Added guava to build.gradle(ServiceLambda), created a CacheStore and CacheConfig in App/src/config

---
+++ Application/src/main/java/com/kenzie/appserver/config/CacheStore.java
@@ -0,0 +1,37 @@
+package com.kenzie.appserver.config;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.w3c.dom.events.Event;
+
+import java.util.concurrent.TimeUnit;
+
+public class CacheStore {
+    private Cache<String, Event> cache;
+
+    public CacheStore(int expiry, TimeUnit timeUnit) {
+            // initalize the cache
+        this.cache = CacheBuilder.newBuilder()
+                    .expireAfterWrite(expiry, timeUnit)
+                    .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                    .build();
+        }
+
+    public Event get(String key) {
+        // Write your code here
+        // Retrieve and return the Event
+        return cache.getIfPresent(key);
+    }
+
+    public void delete(String key) {
+        // Write your code here
+        // Invalidate/evict the Event from cache
+        cache.invalidate(key);
+    }
+
+    public void add(String key, Event value) {
+        // Write your code here
+        // Add Event to cache
+        cache.put(key, value);
+    }
+}
